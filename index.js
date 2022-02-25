dd2d93fef3e171d504f1e090ef3f58b87f859dc6
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-02-25 01:37:53 -0600

    feat: finalizando leccion 3

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ if (paragraphs.length > 0) {
   let paragraph = paragraphs[0];
   paragraph.innerText = "Bienvenidos al Bootcamp";
 }
+
+if (paragraphs.length > 1) {
+  let paragraph = paragraphs[0];
+  const fecha = new Date();
+  paragraph.innerText =
+    "Parrafos en el documento: " + paragraphs.length + " (" + fecha + ")";
+}
