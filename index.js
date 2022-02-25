b3703a691b6a0c972b754b6251999bc9b8437edc
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-02-25 00:48:13 -0600

    feat: cambio en el texto

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let paragraphs = document.getElementsByTagName("p");
-console.log(paragraphs);
+const paragraphs = document.getElementsByTagName("p");
+console.log("Parrafos en el documento " + paragraphs.length);
 
-let paragraph = paragraphs[0];
-paragraph.innerText = "Bienvenidos al Bootcamp";
+if (paragraphs.length > 0) {
+  let paragraph = paragraphs[0];
+  paragraph.innerText = "Bienvenidos al Bootcamp";
+}
