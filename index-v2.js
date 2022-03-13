d77a68a82f8d40f7f4a18ed96fed195f2e8da91c
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-03-13 08:50:00 -0600

    feat: haciendo pruebas con REDUX

--- index-v2.js
+++ index-v2.js
@@ -1,1 +1,37 @@
-console.log(Redux);
+const preloadedState = {
+    producto: {},
+    productos: []
+};
+
+const reducer = (state, action) => {
+    if (action.type == 'producto-agregado') {
+        return {
+            ...state,
+            productos: [
+                ...state.productos, action.payload
+            ]
+        }
+    }
+
+    return state;
+};
+
+const store = Redux.createStore(reducer, preloadedState);
+
+let latestState;
+
+store.subscribe(() => {
+    let currentState = store.getState();
+    if (currentState != latestState) {
+        latestState = currentState;
+        console.log("Estad", store.getState());
+    }
+});
+
+store.dispatch({
+    type: "producto-agregado",
+    payload: {
+        id: 1,
+        nombre: "Prueba a"
+    }
+});
