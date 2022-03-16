0dad59711bb17946136a0ccfddf81c7e4480f869
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-03-15 23:26:10 -0600

    feat: render de la tabla productos, suma de totales

--- index-v2.js
+++ index-v2.js
@@ -1,30 +1,63 @@
 const form = document.getElementsByTagName("form")[0];
-const tbody = document.getElementsByTagName("tbody")[0];
-const cantidadTotalElement = document.getElementById("cantidad-total");
-const precioTotalElement = document.getElementById("precio-total");
-const granTotalElement = document.getElementById("gran-total");
-
 const inputCodigo = document.getElementById("codigo");
 const inputNombre = document.getElementById("nombre");
 const inputCantidad = document.getElementById("cantidad");
 const inputPrecio = document.getElementById("precio");
 const inputCategoria = document.getElementById("categoria");
 
+const tbody = document.getElementsByTagName("tbody")[0];
+const cantidadTotalElement = document.getElementById("cantidad-total");
+const precioTotalElement = document.getElementById("precio-total");
+const granTotalElement = document.getElementById("gran-total");
+
 const preloadedState = {
     producto: {},
     productos: []
 };
+let indice = 0;
 
 const reducer = (state, action) => {
-    if (action.type == 'producto-agregado') {
+    if (action.type == "producto-agregado") {
+        const codigo = indice++;
+        const producto = action.payload;
+        const total = producto.cantidad * producto.precio;
         return {
             ...state,
             productos: [
-                ...state.productos, action.payload
+                ...state.productos,
+                {
+                    ...producto,
+                    codigo,
+                    total
+                }
             ]
         }
     }
+    if (action.type == "producto-modificado") {
+        const producto = action.payload;
+        const productos = state.productos.slice();
+        const codigo = producto.codigo;
+        const total = producto.cantidad * producto.precio;
+        const old = productos.find((item) => item.codigo == codigo);
+        const index = productos.indexOf(old);
+        productos[index] = { ...producto, total };
+        return {
+            ...state,
+            productos
+        }
 
+
+    }
+    if (action.type == "producto-eliminado") {
+        const codigo = action.payload.codigo;
+        const productos = state.productos.filter((item) => item.codigo != codigo);
+        return {
+            ...state,
+            productos
+        }
+
+
+    }
     return state;
 };
 
@@ -32,58 +65,80 @@ const store = Redux.createStore(reducer, preloadedState);
 
 let latestState;
 
-store.subscribe(() => {
+const unsuscribe = store.subscribe(() => {
     let currentState = store.getState();
     if (currentState != latestState) {
         latestState = currentState;
-        console.log("Estad", store.getState());
+        console.log(currentState.productos);
+        renderTable(currentState.productos);
     }
 });
 
 //function para crear nuevas filas en la tabla
 function renderTable(productos) {
 
-
     const filas = productos.map((item) => {
         const tr = document.createElement("tr");
         tr.innerHTML =
             `<td>${item.codigo}</td>
-        <td>${item.nombre}</td>
-        <td>${item.cantidad}</td>
-        <td>${item.precio}</td>
-        <td>${item.categoria}</td>
-        <td>
+            <td>${item.nombre}</td>
+            <td>${item.cantidad}</td>
+            <td>${item.precio}</td>
+            <td>${item.total}</td>
+            <td>
             <div class="btn-group">
-                <a href="#" class="btn btn-small btn-outline-secondary" onClick="onEdit(event)">
+                <a href="#" class="btn btn-small btn-outline-secondary">
                     <i class="bi bi-pencil-square"></i>
                 </a>
-                <a href = "#" onClick = "onDelete(event)" class="btn btn-small btn-outline-danger">
+                <a href="#"  class="btn btn-small btn-outline-danger">
                     <i class='bi bi-trash'></i>
                 </a>
             </div>
         </td>`;
+        const [editar, eliminar] = tr.getElementsByTagName("a");
+        eliminar.addEventListener("click", (event) => {
+            event.preventDefault();
+            store.dispatch({
+                type: "producto-eliminado",
+                payload: {
+                    codigo: item.codigo
+                }
+            })
+        });
         return tr;
     });
+
+    tbody.innerHTML = "";
     filas.forEach((tr) => {
         tbody.appendChild(tr);
     });
 
+    const cantidadTotal = sum(productos, x => x.cantidad);
+
+    const precioTotal = sum(productos, x => x.precio);
+
+    const granTotal = sum(productos, x => x.total);
+
+    cantidadTotalElement.innerText = cantidadTotal;
+    precioTotalElement.innerText = precioTotal;
+    granTotalElement.innerText = granTotal;
+
+    function sum(elem, selector) {
+        return elem.map(selector).reduce((a, b) => a + b, 0);
+    }
+
+
 }
+
+
 
 store.dispatch({
     type: "producto-agregado",
     payload: {
-        id: 1,
-        nombre: "Prueba a"
-    }
-});
-
-//modificado
-store.dispatch({
-    type: "producto-modificado",
-    payload: {
-        id: 1,
-        nombre: "Prueba a-v2"
+        nombre: "Prueba a",
+        cantidad: 3,
+        precio: 10,
+        categoria: 2
     }
 });
 
@@ -92,14 +147,39 @@ store.dispatch({
 store.dispatch({
     type: "producto-agregado",
     payload: {
-        id: 2,
-        nombre: "Prueba b"
+        nombre: "Prueba b",
+        cantidad: 10,
+        precio: 5.5,
+        categoria: 1
+    }
+});
+
+//modificado
+store.dispatch({
+    type: "producto-modificado",
+    payload: {
+        codigo: 1,
+        nombre: "Prueba b-v2",
+        cantidad: 11,
+        precio: 12,
+        categoria: 2
     }
 });
 
 store.dispatch({
-    type: "producto-eliminado",
+    type: "producto-agregado",
     payload: {
-        id: 1
+        nombre: "Prueba c",
+        cantidad: 2,
+        precio: 4,
+        categoria: 4
     }
 });
+
+
+/*store.dispatch({
+    type: "producto-eliminado",
+    payload: {
+        codigo: 2
+    }
+});*/
