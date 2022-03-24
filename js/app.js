e52bbd8912cdb84a84c49c395462e840dbc83763
Vladimir Salguero <vladimir891@hotmail.com>
2022-03-24 13:34:24 -0600

    feat: add function dispatch - end video 14 Lógica en Middlewares

--- js/app.js
+++ js/app.js
@@ -11,19 +11,26 @@ const middlewares = Redux.applyMiddleware(
 );
 const store = Redux.createStore(reducer, preloadedState, middlewares);
 
-let latestState;
-
-const subscribe = store.subscribe(() => {
-    let currentState = store.getState();
-    if (currentState != latestState) {
-        latestState = currentState;
-        ui.renderForm(currentState.producto);
-        ui.renderTable(currentState.productos);
-    }
-});
+store.subscribe(dispatchOnChange(store, (state) =>{
+  ui.renderForm(state.producto);
+  ui.renderTable(state.productos);
+}))
 
 ui.onFormSubmit = (producto) => store.dispatch(agregarOModificarProducto(producto));
 
 ui.onEliminarClick = (codigo) => store.dispatch(productoEliminado(codigo));
 
 ui.onEditarClick = (codigo) => store.dispatch(productoSeleccionado(codigo));
+
+function dispatchOnChange(store, dispatch){
+    let latestState;
+
+    return function(){
+        let currentState = store.getState();
+        if (currentState != latestState) {
+            latestState = currentState;
+            dispatch(latestState);
+        }
+    }
+
+}
