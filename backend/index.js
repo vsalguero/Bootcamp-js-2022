a70b1e0c43f896f18344533e28bca17018486342
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-16 00:04:22 -0600

    feat: endpoint update

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,72 @@
-import http from "http";
-//const http = require("http");
+import express from "express";
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type" : "application/json"});
-    res.write(JSON.stringify([
-        {
-            codigo: 1, 
-            nombre: "producto 1", 
-            precio: 10, 
-            cantidad: 2
-        },
-        {
-            codigo: 2, 
-            nombre: "producto 2", 
-            precio: 10, 
-            cantidad: 2
-        }
-    ]));
-    res.end();
+let lastId = 1;
+const productos = [
+    {
+        nombre: "producto a",
+        cantidad : 2,
+        precio : 10,
+        codigo : lastId
+    }
+];
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hola mundo</h1>");
 });
 
-server.listen(5000, ()=>{
-    console.log("Servidor escuchando en el puerto 5000");
+//Globar middleware for all request
+app.use(logs);
+//app.use(bodyParser.json({type: 'application/json'}));
+app.use(express.json());
+
+app.get("/productos", (req, res) => {
+    res.json(productos);
 });
 
+app.post("/productos", (req, res) => {
+    console.log("Body:", req.body);
+    lastId++;
+    const producto = { ...req.body, codigo: lastId };
+    productos.push(producto);
+    res.status(201);
+    res.json(producto);
+});
+
+app.put("/productos/:codigo", (req, res) => {
+    const codigo = parseInt(req.params.codigo);
+    const producto = productos.find(p => p.codigo == codigo);
+    if (codigo) {
+        const index = productos.indexOf(producto);
+        const nuevoProducto = productos[index] = { ...req.body, codigo }
+        res.status(200);
+        res.json(nuevoProducto);
+    } else {
+        res.status(404);
+        res.json({ message: "No existe ningun producto con ese codigo" + codigo });
+    }
+});
+
+
+//middleware autenticate
+/*function isAuthenticated(req, res, next) {
+    const auth = req.headers.authorization;
+    if(auth == "holamundo"){
+        next();
+    }else{
+        res.status(401);
+        res.send("Not authorized");
+    }
+}*/
+
+//global middleware logs
+function logs(req, res, next) {
+    console.log(`${req.method}: ${req.originalUrl}`);
+    next();
+}
+
+
+
+app.listen(5000, () => {
+    console.log("Escuchando en el puerto 5000");
+});
