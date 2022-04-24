e31ef2ed4f628554dfe5bbf81a396792ce06b13a
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-24 11:13:31 -0600

    feat: import productos from mongodb model

--- backend/database/mongodb/index.js
+++ backend/database/mongodb/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import productos from "./productos";
 
-mongoose.connect("mongodb:localhost/productos", () =>{
+mongoose.connect("mongodb+srv://vsalguero:74gIL7iXlsogZojV@cluster0.4yq0r.mongodb.net/BootcampDB?retryWrites=true&w=majority", () =>{
     console.log("Conectado a la base de datos");
 });
 
-export default {productos};
+export default { productos };
 
--- backend/database/mongodb/productos.js
+++ backend/database/mongodb/productos.js
@@ -1,31 +1,29 @@
 import mongoose from "mongoose";
 
-
-const ProductosScheme = new mongoose.Schema(
-  {
-    nombre: {
-        type: String,
-        required: true,
-      },
-      categoria: {
-        type: Number,
-        required: true
-      },
-      cantidad: {
-        type: Number,
-        required: true
-      },
-      precio: {
-        type: Number,
-        required: true
-      },
-      total: {
-        type: Number,
-        required: true
-      },
+const ProductosScheme = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
   },
+  categoria: {
+    type: Number,
+    required: true
+  },
+  cantidad: {
+    type: Number,
+    required: true
+  },
+  precio: {
+    type: Number,
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
+  },
+},
   {
-    timestamps: true, //TODO: createdAt, updatedAt
+    timestamps: false, //TODO: createdAt, updatedAt
     versionKey: false,
   }
 );
@@ -36,33 +34,33 @@ const Productos = mongoose.model("productos", ProductosScheme);
 //methods
 const all = () => Productos.find({}).then(mapProductos);
 
-const filter = ()=> Productos.find({ nombre: {$regex: filtro, $options: "i"} }).then(mapProductos);
+const filter = (filtro) => Productos.find({ nombre: { $regex: filtro, $options: "i" } }).then(mapProductos);
 const add = (producto) => {
-    const nuevoProducto = new Productos({...producto, total: producto.cantidad * producto.precio });
-    return nuevoProducto.save().then(mapProducto);
+  const nuevoProducto = new Productos({ ...producto, total: producto.cantidad * producto.precio });
+  return nuevoProducto.save().then(mapProducto);
 };
 
-const single = (_id) => Productos.findOne({_id});
-const update = (_id, producto) => Productos.findOneAndUpdate({_id}, producto, {new: true}).then(mapProducto);;
-const remove = (_id) => Productos.findOneAndRemove({_id}).then(mapProducto);;
+const single = (_id) => Productos.findOne({ _id });
+const update = (_id, producto) => Productos.findOneAndUpdate({ _id }, producto, { new: true }).then(mapProducto);
+const remove = (_id) => Productos.findOneAndRemove({ _id }).then(mapProducto);;
 
-function mapProductos(productos){
-  return productos.map(p => ({...p.toJSON(), codigo: _id}));
+function mapProductos(productos) {
+  return productos.map(p => ({ ...p.toJSON(), codigo: _id }));
 }
 
-function mapProducto(producto){
-  if(producto){
-    return {...producto.toJSON(), codigo: producto._id};
+function mapProducto(producto) {
+  if (producto) {
+    return { ...producto.toJSON(), codigo: producto._id };
   }
   return null;
 }
 
 
 export default {
-    all, 
-    filter,
-    add,
-    single,
-    update,
-    remove
+  all,
+  filter,
+  add,
+  single,
+  update,
+  remove
 }
